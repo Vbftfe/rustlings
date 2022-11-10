b2c546d17dfd62f5047ba3ab1905f3e32c662ffd
Vbftfe <1295125020@qq.com>
2022-11-10 20:21:55 +0800

    完成hashmaps练习

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -40,8 +38,21 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        add_team(&mut scores, team_1_name, team_1_score, team_2_score);
+        add_team(&mut scores, team_2_name, team_2_score, team_1_score);
     }
     scores
+}
+
+fn add_team(scores: &mut HashMap<String, Team>, name: String, score1: u8, score2: u8) {
+    let team = scores.entry(name.clone()).or_insert(Team {
+        name: name,
+        goals_scored: 0,
+        goals_conceded: 0,
+    });
+
+    team.goals_scored += score1;
+    team.goals_conceded += score2;
 }
 
 #[cfg(test)]
